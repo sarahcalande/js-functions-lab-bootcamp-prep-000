3b4724f8c88eb213a44706a53241638b4d237392
scalande@yahoo.com <scalande@yahoo.com>
2018-06-11 11:34:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(name){
+  return `Happy holidays, ${name}!`
+}
